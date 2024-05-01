40ea03d7657ffc5a5321d0dee5585f6186193df0
and-is <andispaudel@gmail.com>
2024-05-01 13:27:27 +0545

    did palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let onlyStr = str.toLowerCase();
+  onlyStr = onlyStr.replace(/[^a-zA-Z0-9]/g, "");
+  return onlyStr === onlyStr.split("").reverse().join("");
 };
 
 // Do not edit below this line
