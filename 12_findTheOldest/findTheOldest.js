f1d398cf737347de8d34f9533ba3128a39b87b20
and-is <andispaudel@gmail.com>
2024-05-01 13:45:57 +0545

    did all

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function (str) {
+  return str.reduce((oldest, a) => {
+    const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+    const currentAge = getAge(a.yearOfBirth, a.yearOfDeath);
+    return oldestAge < currentAge ? a : oldest;
+  });
+};
 
+const getAge = function (birth, death) {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
 };
 
 // Do not edit below this line
